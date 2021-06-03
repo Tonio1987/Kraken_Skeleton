9985976afc1932cadf6cbec2a05bcc518e1bcbc4
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-03 15:16:41 +0200

    mysal bugfix

--- persistence/cron/DB_CronTask.js
+++ persistence/cron/DB_CronTask.js
@@ -5,13 +5,19 @@ moment.locale('fr');
 module.exports = {
     getCronTasks: function (callback, db_pool) {
         new Promise(function (resolve, reject) {
-            db_pool.query('SELECT * FROM TR_CRON_TASKS_CTK', function (error, results, fields) {
+			pool.getConnection(function(error, connection) {
 				if (error){
 					reject(error);
-				}	
-				console.log('The results is: ', results);
-				resolve(result);
-			});	
+				}
+				connection.query('SELECT * FROM TR_CRON_TASKS_CTK', function (error, results, fields) {
+					connection.release();
+					if (error){
+						reject(error);
+					}
+					console.log('The results is: ', results);
+					resolve(result);
+				});
+			});
         }).then(function(data){
             callback(null, data);
         }).catch(function(err) {
