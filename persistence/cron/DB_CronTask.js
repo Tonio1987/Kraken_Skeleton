46f9d1ab6c13f4b1f7bf09e3569dab4bdf28c33a
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-07 10:35:45 +0200

    test wth promise just callback mysql

--- persistence/cron/DB_CronTask.js
+++ persistence/cron/DB_CronTask.js
@@ -5,26 +5,23 @@ moment.locale('fr');
 module.exports = {
     getCronTasks: function (callback, db) {
 		console.log("db is a " + db);
-        new Promise(function (resolve, reject, db) {
-			console.log("enter promise");
-			db(function (err, con) {
+		db(function (err, con) {
+			if(err) {
+				console.log("err: " + err); 
+				callback(null, err);
+			}
+			var userQuery = 'select * from TR_CRON_TASKS_CTK';
+			console.log("con: " + con); 
+			con.query(userQuery,function(err,res){
 				if(err) {
 					console.log("err: " + err); 
+					callback(null, err);
 				}
-				var userQuery = 'select * from TR_CRON_TASKS_CTK';
-				console.log("con: " + con); 
-				con.query(userQuery,function(err,res){
-					console.log(res);
-					con.release();
-					resolve(res);
-				});
+				console.log(res);
+				con.release();
+				callback(res, null);
 			});
-        }).then(function(data){
-			console.log("then");
-            callback(null, data);
-        }).catch(function(err) {
-			console.log("ERROR");
-            callback(err, null);
-        });
+		});
+
     }
 };
