0ea24093f1c0c610cd72657e7310f9a14354c32c
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-07 11:57:01 +0200

    test

--- persistence/cron/DB_CronTask.js
+++ persistence/cron/DB_CronTask.js
@@ -7,12 +7,14 @@ module.exports = {
 		var getConnection = require('../../config/db_mysql_config');
 		getConnection(function (err, con) {
 			if(err) { 
+				console.log("error on con: " + err); 
 				callback(err, null);
 			}
 			var userQuery = 'SELECT * FROM TR_CRON_TASKS_CTK;';
+			console.log(userQuery);
 			con.query(userQuery, function(err, res){
 				if(err) {
-					console.log("error here: " + err); 
+					console.log("error on query: " + err); 
 					callback(err, null);
 				}
 				console.log("RESULT IS : " + res);
