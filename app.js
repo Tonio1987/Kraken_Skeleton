91d7d5f253ec6ed55718f62ff08404985f54b356
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-07 09:21:52 +0200

    test db

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ require('dotenv').config();
 const cron = require('node-cron');
 
 // INIT MYSQL MODULE
-const db = require('./config/db_mysql_config');
+//const db = require('./config/db_mysql_config');
+
+const db = mysql.createConnection({
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASSWORD,
+  database        : process.env.DB_DATABASE
+ });
+ 
+db.connect(function(err) {
+   if (err) throw err;
+   console.log("Connecté à la base de données MySQL!");
+ });
 
 // CALL WEB MODULES
 var express = require('express');
