42c2f527356624fa70c58c67a6cdfedd6cb714ee
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-07 12:39:51 +0200

    test

--- persistence/cron/DB_CronTask.js
+++ persistence/cron/DB_CronTask.js
@@ -13,7 +13,7 @@ module.exports = {
 			var userQuery = 'SELECT * FROM TR_CRON_TASKS_CTK';
 			console.log(userQuery);
 			
-			con.query(userQuery, function(err, res){
+			var result = con.query(userQuery, function(err, res){
 				if(err) {
 					console.log("error on query: " + err); 
 					callback(err, null);
@@ -21,8 +21,9 @@ module.exports = {
 				console.log("RESULT IS : " + res);
 				con.release();
 				
-				callback(err, res);
+				return res;
 			});
+			callback(null, result);
 		});
 	}
 };
