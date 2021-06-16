e25360a0508c18acb7f41f2b99523fbbc3a5981e
Anthony AVOLA <anthony.avola@gmail.com>
2021-06-16 17:53:44 +0200

    bugfix inCRON Schedulmer

--- cron/CRON_scheduler.js
+++ cron/CRON_scheduler.js
@@ -13,7 +13,7 @@ let Handler={};
 // CONTROLLER CALL
 // KRAKEN API
 const CTRL_Time = require('../controller/api/kraken/CTRL_Time');
-const CTRL_Time = require('../controller/api/kraken/CTRL_AssetPairs');
+const CTRL_AssetPairs = require('../controller/api/kraken/CTRL_AssetPairs');
 
 // INIT TASKS ATTRIBUTES
 // SERVER CHECK TASKS
